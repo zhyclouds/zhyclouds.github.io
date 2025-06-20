de63ad94b76652617bc6a32afb1dc37a2f0b7fc7
zhyclouds <310123665@qq.com>
2025-06-20 13:39:49 +0800

    Site updated: 2025-06-20 13:39:48

---
+++ css/statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+$(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1db09ba0639a1743b2d5b317cd60a7d9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
